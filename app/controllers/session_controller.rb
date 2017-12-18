c128450d85b7ce90799adce053349e9c9ff0465a
chriswillphoto <christophernekich@gmail.com>
2017-12-18 22:05:14 +1100

    dno

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,7 +3,7 @@ class SessionController < ApplicationController
   def create
     user = User.find_by :email => params[:email]
     if user.present? && user.authenticate(params[:password])
-    end
+
   end
 
 
